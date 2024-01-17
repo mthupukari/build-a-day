9721ab1661cba102a76e231f6d5a3c1b2cd48471
Mahit Thupukari <mthupukari@mahitthkarismbp.lan>
2024-01-17 02:07:34 -0500

    working game

--- tic tac toe/big_board.py
+++ tic tac toe/big_board.py
@@ -7,10 +7,17 @@ class BigBoard:
         self.screen = pygame.display.set_mode((600, 600))
         self.screen.fill((255, 255, 255))
         self.small_boards = []
+        self.game_board = [[' '] * 3 for _ in range(3)]
 
         self.big_cell_size = 200  
         self.margin = 5  
-        self.small_cell_size = (self.big_cell_size - 2 * self.margin) // 3  
+        self.small_cell_size = (self.big_cell_size - 2 * self.margin) // 3 
+
+        self.current_player = 'x'
+
+        self.index_map = {0 : (0,0), 1 : (0, 1), 2 : (0, 2),
+                          3 : (1,0), 4 : (1, 1), 5 : (1, 2),
+                          6 : (2,0), 7 : (2, 1), 8 : (2, 2)}
 
         for row in range(3):
             for col in range(3):
@@ -20,7 +27,91 @@ class BigBoard:
                 
                 self.small_boards.append(SmallBoard(self.screen, top_left, self.small_cell_size))
 
+    def handle_mouse_click(self, pos):
+        x, y = pos
+
+        board_col = x // (self.big_cell_size + self.margin)
+        board_row = y // (self.big_cell_size + self.margin)
+        small_board_index = board_row * 3 + board_col
+        small_board = self.small_boards[small_board_index]
+
+        cell_x = x % (self.big_cell_size + self.margin)  
+        cell_y = y % (self.big_cell_size + self.margin)  
+        cell_col = cell_x // self.small_cell_size
+        cell_row = cell_y // self.small_cell_size
+
+        if small_board.is_valid_move(cell_row, cell_col):
+            if small_board.make_move(cell_row, cell_col, self.current_player):
+                self.switch_player()
+
+        verdict = small_board.check_winner()
+
+        x, y = self.index_map[small_board_index]
+
+        if verdict == 'x':
+            self.game_board[x][y] = 'x'
+        elif verdict == 'o':
+            self.game_board[x][y] = 'o'
+        elif verdict == 'e':
+            self.game_board[x][y] = 'e'
+
+        print(self.game_board)
+
+    def switch_player(self):
+        self.current_player = 'x' if self.current_player == 'o' else 'o'
+
+    def check_big_board_winner(self):
+        # Check if X won
+        for i in range(3):
+            if self.game_board[i][0] == 'x' and self.game_board[i][1] == 'x' and self.game_board[i][2] == 'x':
+                return 'x'
+        for i in range(3):
+            if self.game_board[0][i] == 'x' and self.game_board[1][i] == 'x' and self.game_board[2][i] == 'x':
+                return 'x'
+        if self.game_board[0][0] == 'x' and self.game_board[1][1] == 'x' and self.game_board[2][2] == 'x':
+            return 'x'
+        elif self.game_board[0][2] == 'x' and self.game_board[1][1] == 'x' and self.game_board[2][0] == 'x':
+            return 'x'
+        
+        #Check if O won
+        for i in range(3):
+            if self.game_board[i][0] == 'o' and self.game_board[i][1] == 'o' and self.game_board[i][2] == 'o':
+                return 'o'
+        for i in range(3):
+            if self.game_board[0][i] == 'o' and self.game_board[1][i] == 'o' and self.game_board[2][i] == 'o':
+                return 'o'
+        if self.game_board[0][0] == 'o' and self.game_board[1][1] == 'o' and self.game_board[2][2] == 'o': 
+            return 'o'
+        elif self.game_board[0][2] == 'o' and self.game_board[1][1] == 'o' and self.game_board[2][0] == 'o': 
+            return 'o'
+        
+        #check end
+        end = True
+        for i in range(3):
+            if not end:
+                break
+            for j in range(3):
+                if self.game_board[i][j] != 'e':
+                    end = False
+                    break
+        if end:
+            return 'e'
+        #Check if board is full
+        full = True
+        for i in range(3):
+            if not full:
+                break
+            for j in range(3):
+                if self.game_board[i][j] == ' ':
+                    full = False
+                    break
+        if full:
+            return 'f'
+        
+        return None
+
     def run(self):
+        turn = True
         running = True
         while running:
             self.screen.fill((255, 255, 255))  # Clear the screen for redrawing
@@ -42,6 +133,14 @@ class BigBoard:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.handle_mouse_click(pos = pygame.mouse.get_pos())
+                check = self.check_big_board_winner()
+                if check != None:
+                    print(check)
+                    running = False
+                    
 
             pygame.display.flip()
 
--- tic tac toe/small_board.py
+++ tic tac toe/small_board.py
@@ -8,25 +8,89 @@ class SmallBoard:
         self.cell_size = cell_size
         self.x_png = pygame.transform.scale(pygame.image.load("x.png"), (self.cell_size, self.cell_size))
         self.o_png = pygame.transform.scale(pygame.image.load("o.png"), (self.cell_size, self.cell_size))
+        self.cover = ''
 
+    def make_move(self, row, col, player):
+        if self.is_valid_move(row, col):
+            self.board[row][col] = player
+            return True
+        return False
+
+    def is_valid_move(self, row, col):
+        return self.board[row][col] == ' '  
+
+    def check_winner(self):
+
+        # Check if X won
+        for i in range(3):
+            if self.board[i][0] == 'x' and self.board[i][1] == 'x' and self.board[i][2] == 'x':
+                self.cover = 'x'
+                return 'x'
+        for i in range(3):
+            if self.board[0][i] == 'x' and self.board[1][i] == 'x' and self.board[2][i] == 'x':
+                self.cover = 'x'
+                return 'x'
+        if self.board[0][0] == 'x' and self.board[1][1] == 'x' and self.board[2][2] == 'x':
+            self.cover = 'x'
+            return 'x'
+        elif self.board[0][2] == 'x' and self.board[1][1] == 'x' and self.board[2][0] == 'x':
+            self.cover = 'x'
+            return 'x'
+        
+        #Check if O won
+        for i in range(3):
+            if self.board[i][0] == 'o' and self.board[i][1] == 'o' and self.board[i][2] == 'o':
+                self.cover = 'o'
+                return 'o'
+        for i in range(3):
+            if self.board[0][i] == 'o' and self.board[1][i] == 'o' and self.board[2][i] == 'o':
+                self.cover = 'o'
+                return 'o'
+        if self.board[0][0] == 'o' and self.board[1][1] == 'o' and self.board[2][2] == 'o': 
+            self.cover = 'o'
+            return 'o'
+        elif self.board[0][2] == 'o' and self.board[1][1] == 'o' and self.board[2][0] == 'o': 
+            self.cover = 'o'
+            return 'o'
+        
+        #Check if board is full
+        full = True
+        for i in range(3):
+            if not full:
+                break
+            for j in range(3):
+                if self.board[i][j] == ' ':
+                    full = False
+                    break
+
+        return 'e' if full else None
+    
     def render(self):
-        x_start, y_start = self.top_left
-        small_board_size = self.cell_size * 3  
+        if self.cover != '':
+            if self.cover == 'x':
+                x_png = pygame.transform.scale(self.x_png, (200, 200))
+                self.surface.blit(x_png, self.top_left)
+            else:
+                o_png = pygame.transform.scale(self.o_png, (200, 200))
+                self.surface.blit(o_png, self.top_left)
+        else:
+            x_start, y_start = self.top_left
+            small_board_size = self.cell_size * 3  
 
-        for col in range(1, 3):
-            x = x_start + col * self.cell_size
-            pygame.draw.line(self.surface, (0, 0, 0), (x, y_start), (x, y_start + small_board_size))
+            for col in range(1, 3):
+                x = x_start + col * self.cell_size
+                pygame.draw.line(self.surface, (0, 0, 0), (x, y_start), (x, y_start + small_board_size))
 
-        for row in range(1, 3):
-            y = y_start + row * self.cell_size
-            pygame.draw.line(self.surface, (0, 0, 0), (x_start, y), (x_start + small_board_size, y))
+            for row in range(1, 3):
+                y = y_start + row * self.cell_size
+                pygame.draw.line(self.surface, (0, 0, 0), (x_start, y), (x_start + small_board_size, y))
 
-        for i, row in enumerate(self.board):
-            for j, cell in enumerate(row):
-                cell_center_x = x_start + j * self.cell_size + self.cell_size // 2
-                cell_center_y = y_start + i * self.cell_size + self.cell_size // 2
+            for i, row in enumerate(self.board):
+                for j, cell in enumerate(row):
+                    cell_center_x = x_start + j * self.cell_size + self.cell_size // 2
+                    cell_center_y = y_start + i * self.cell_size + self.cell_size // 2
 
-                if cell == 'X':
-                    self.surface.blit(self.x_png, (cell_center_x - self.x_png.get_width() // 2, cell_center_y - self.x_png.get_height() // 2))
-                elif cell == 'O':
-                    self.surface.blit(self.o_png, (cell_center_x - self.o_png.get_width() // 2, cell_center_y - self.o_png.get_height() // 2))
+                    if cell == 'x':
+                        self.surface.blit(self.x_png, (cell_center_x - self.x_png.get_width() // 2, cell_center_y - self.x_png.get_height() // 2))
+                    elif cell == 'o':
+                        self.surface.blit(self.o_png, (cell_center_x - self.o_png.get_width() // 2, cell_center_y - self.o_png.get_height() // 2))
